48cc1394a33b9e3ff4f885e2f458e87bddbad9f5
shin <wotla3009@naver.com>
2023-03-17 18:03:30 +0900

    scott 수정

--- scott.sql
+++ scott.sql
@@ -1,1 +1,86 @@
-#scott
+--scott
+
+--select : 데이터 조회
+
+--emp(employee) 테이블 구성 보기
+--   필드명(열이름)       제약조건       데이터타입
+--  EMPNO(사원번호)      NOT NULL      NUMBER(4)
+--  ENAME(사원명), JOB(직책), MGR(직속상관 번호), HIREDATE(입사일), SAL(급여), COMM(수당), DEPTNO(부서번호)
+-- NUMBER : 소수점 자릿수를 포함해서 지정 가능
+-- NUMBER(4) : 4자리 숫자까지 허용, NUMBER(8,2) : 전체 자릿수는 8 자리이고 소수점 2자리를 포함한다
+-- VARCHAR2 : 가변형 문자열 저장
+-- VARCHAR2(10) : 10byte 문자까지 저장 가능
+-- DATE : 날짜 데이터
+DESC emp;
+
+--DEPTNO(부서번호), DNAME(부서명), LOC(부서위치)
+DESC dept;
+
+--GRADE(급여등급), LOSAL(최소 급여액), HISAL(최대 급여액)
+DESC salgrade;
+
+--select : 데이터 조회  !!!중요!!!가장 많이 사용!!!
+--조회 방식 : 셀렉션(행 단위로 조회), 프로젝션(열 단위로 조회), 조인(두개 이상의 테이블을 사용하여 조회)
+--SELECT 열이름1,열이름2... EMP;(조회할 열이 전체라면 *로 처리)
+--FROM 테이블명;
+
+--1.EMP 테이블의 전체 열을 조회
+SELECT
+    *
+FROM
+    emp;
+
+--2.EMP 테이블에서 사원번호, 이름, 급여 열을 조회
+SELECT
+    empno,
+    ename,
+    sal
+FROM
+    emp;
+
+--3. DEPT 테이블 전체 조회
+SELECT
+    *
+FROM
+    dept;
+--4. DEPT 테이블안에 부서번호, 지역만 조회
+SELECT
+    deptno,
+    loc
+FROM
+    dept;
+--5. EMP 테이블안에 부서번호
+SELECT
+    deptno
+FROM
+    emp;
+--6. EMP 테이블안에서 부서번호 조회(단, 중복된 부서 번호는 제거)
+SELECT DISTINCT
+    deptno
+FROM
+    emp;
+ 
+ --열이 여러 개인 경우 (묶어서 중복이냐 아니냐를 판단)   
+SELECT DISTINCT
+    job,
+    deptno
+FROM
+    emp;    
+
+--7. 연산
+--별칭 : 필드에 별칭을 임의로 부여 ( AS 별칭, 혹은 한칸 띄고 별칭, 별칭에 공간이 있다면 ""로 묶어주기)
+-- 사원들의 1년 연봉 구하기 ( SAL * 12 + COMM )
+SELECT
+    empno,
+    ename,
+    sal * 12 + comm AS annsal
+FROM
+    emp;
+
+SELECT
+    empno,
+    ename           사원명,
+    job             "직 책",
+    sal * 12 + comm annsal
+FROM
+    emp;
--- system.sql
+++ system.sql
@@ -1,23 +1,34 @@
-# 대소문자 구별 하지 않음(단, 비밀번호는 대소문자 구별함)
-# CREATE : 생성 / ALTER : 수정 / DROP : 삭제 / DELETE : 삭제
-
-# 오라클 버전이 변경되면서 사용자 생성 시 C## 문자를 넣어서  만들도록 변경 됨
-# JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
-ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
-
-# user 생성은 sys, system 만 가능
-# user 생성 (공간 할당)
-# CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
+--# 대소문자 구별 하지 않음(단, 비밀번호는 대소문자 구별함)
+--# CREATE : 생성 / ALTER : 수정 / DROP : 삭제 / DELETE : 삭제
+--
+--# 오라클 버전이 변경되면서 사용자 생성 시 C## 문자를 넣어서  만들도록 변경 됨
+--# JAVADB => C##JAVADB 이런 방식을 C## 사용하지 않겠음
+--ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+--
+--# user 생성은 sys, system 만 가능
+--# user 생성 (공간 할당)
+--# CREATE USER 사용자이름 IDENTIFIED BY 비밀번호
 CREATE USER JAVADB IDENTIFIED BY 12345
-DEFAULT TABLESPACE USERS   #user 생성 공간 할당
-TEMPORARY TABLESPACE TEMP;     #user 생성 공간 할당
+DEFAULT TABLESPACE USERS  -- #user 생성 공간 할당
+TEMPORARY TABLESPACE TEMP;     --#user 생성 공간 할당
 
-# GRANT : 원한 부여(사용자 생성만 해서는 아무것도 할수 없음)
+--# GRANT : 원한 부여(사용자 생성만 해서는 아무것도 할수 없음)
 GRANT CONNECT, RESOURCE TO JAVADB;
 
 
 CREATE USER SCOTT IDENTIFIED BY TIGER
-  2  DEFAULT TABLESPACE USERS
-  3  TEMPORARY TABLESPACE TEMP;
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP;
   
    GRANT CONNECT, RESOURCE TO SCOTT;
+   
+   
+   select * from all_users;
+   
+GRANT UNLIMITED TABLESPACE TO SCOTT;   
+   
+   
+GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
+ALTER USER SCOTT DEFAULT TABLESPACE USERS;
+ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
+   
